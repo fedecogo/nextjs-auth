91d9ea339467a15a560a1f37b4eee3a24f243bdf
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-08-20 11:13:19 +0200

    middleware,cookies,npm i cookie

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
 import { fetchAuthUserAction } from "@/actions";
 import Logout from "@/components/log-out/page";
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function Home() {
   const currentUser = await fetchAuthUserAction();
 
-  console.log(currentUser);
-  if (!currentUser?.success) redirect("/sign-in");
+  if (!currentUser?.success) {
+    redirect("/sign-in");
+  }
+  const cookieStore = cookies();
+  const token = cookieStore.get("token"); 
 
   return (
     <div className="text-center mt-5">
       <h1>Next JS Authentication</h1>
       <h2 className="mt-5">Benvenuto {currentUser?.data?.userName}</h2>
-      <p className="mb-5">La password salvata a db con bcrypt: {currentUser?.data?.password}</p>
+      <p>Ecco il tuo token salvato nei cookie: {token?.value}</p>
+      <p className="mt-5 mb-5">Ed ecco la password salvata nel db con bcrypt: {currentUser?.data?.password}</p>
       <Logout />
     </div>
   );
